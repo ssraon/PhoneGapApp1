4d7d620fa50e99967d7aede38d55c3bfe28737e9
Samba Nagabhyru <ssrao_n@yahoo.com>
2014-02-03 17:14:39 -0600

    refactored to old

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -1,27 +1,27 @@
 var EmployeeView = function(employee) {
-
-     this.initialize = function() {
-        this.el = $('<div/>');
-		this.el.on('click', '.add-location-btn', this.addLocation);
-		this.el.on('click', '.change-pic-btn', this.changePicture);
-    };
+ 
+    
 	this.render = function() {
-		this.el.html(EmployeeView.template(employee));
-		return this;
-	};	
-	 this.addLocation = function(event) {
+	this.el.html(EmployeeView.template(employee));
+	this.el.on('click', '.change-pic-btn', this.changePicture);
+	return this;
+	};
+	this.addToContacts = function(event) {
 		event.preventDefault();
-		console.log('addLocation');
-		navigator.geolocation.getCurrentPosition(
-			function(position) {
-				$('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
-			},
-			function() {
-				alert('Error getting location');
-			});
+		console.log('addToContacts');
+		if (!navigator.contacts) {
+			app.showAlert("Contacts API not supported", "Error");
+			return;
+		}
+		var contact = navigator.contacts.create();
+		contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+		var phoneNumbers = [];
+		phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+		phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
+		contact.phoneNumbers = phoneNumbers;
+		contact.save();
 		return false;
 	};
-	
 	this.changePicture = function(event) {
 		event.preventDefault();
 		if (!navigator.camera) {
@@ -44,8 +44,13 @@ var EmployeeView = function(employee) {
 			options);
 	 
 		return false;
-	};	
+	};
+	this.initialize = function() {
+        this.el = $('<div/>');
+		this.el.on('click', '.add-contact-btn', this.addToContacts);
+    };
+ 
     this.initialize();
  
-}
-EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
+ }
+ EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
--- js/HomeView.js
+++ js/HomeView.js
@@ -1,51 +1,29 @@
 var HomeView = function(store) {
-    this.initialize = function() {
+     this.initialize = function() {
         // Define a div wrapper for the view. The div wrapper is used to attach events.
         this.el = $('<div/>');
-        this.el.on('keyup', '#byName', this.findByName);
-		this.el.on('keyup', '#byId', this.findById);
+        this.el.on('keyup', '.search-key', this.findByName);
+		
     };
-	
 	this.render = function() {
 		this.el.html(HomeView.template());
 		return this;
-	};
-	
-	this.findByName = function() {
+	};	
+	 this.findByName = function() {
+	 var self=this;
 		store.findByName($('.search-key').val(), function(employees) {
 			$('.employee-list').html(HomeView.liTemplate(employees));
 			if (self.iscroll) {
-				console.log('Refresh iScroll');
-				self.iscroll.refresh();
-			} else {
-				console.log('New iScroll');
-				self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
-			}			
+						console.log('Refresh iScroll');
+						self.iscroll.refresh();
+					} else {
+						console.log('New iScroll');
+						self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+					}			
 		});
 	};
-	this.findById = function() {
-$.ajax({
-    type: "GET",
-    url: "http://www.thomas-bayer.com/sqlrest/CUSTOMER/2",
-    dataType: "XML",
-    crossDomain: true,
-    contentType: "application/XML; charset=utf-8",
-    success: function (res) {
-        alert("XML: it works!"+res);
-		$('.employee-id-info').html(HomeView.idTemplate(res));
-	//	alert("XML: it works!"+res.content)
-    },
-    error: function (res) {
-        alert("JSON: not working! " + res.statusText);
-    }
-});		
-
-	};	
- 
     this.initialize();
-
-HomeView.template = Handlebars.compile($("#home-tpl").html());
-HomeView.liTemplate = Handlebars.compile($("#employee-li-tpl").html());
-HomeView.idTemplate = Handlebars.compile($("#home-byId-tpl").html());
  
 }
+HomeView.template = Handlebars.compile($("#home-tpl").html());
+HomeView.liTemplate = Handlebars.compile($("#employee-li-tpl").html());
